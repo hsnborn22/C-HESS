ad905c1c89049e1f11d80f9731aa1f14baa36664
heisenborn22 <146590127+heisenborn22@users.noreply.github.com>
2023-10-03 15:27:15 +0200

    Commit #13
    Added a better main menu and a turn system.

--- code/chess.c
+++ code/chess.c
@@ -8,9 +8,11 @@ int * calculateAllowedMovesKnight(int rows, int cols, int *board, int rowPositio
 int * calculateAllowedMovesTower(int rows, int cols, int *board, int rowPosition, int columnPosition, int code);
 int * calculateMovesPiece(int rows, int cols, int *board, int rowPosition, int columnPosition, int code);
 int checkIfMoveIsIn(int rowpos, int columnpos, int *moves, int movesLength);
-void movePiece(int initRow, int initColumn, int endRow, int endColumn, int * board, int code);
+void movePiece(int initRow, int initColumn, int endRow, int endColumn, int * board, int code, int * turn);
 int lookForWhiteCheck(int rows, int cols, int *board);
 int lookForBlackCheck(int rows, int cols, int *board);
+void generateMenu();
+
 
 
 /*  ------------  Pieces notation: ------------
@@ -34,10 +36,32 @@ int main(void) {
 	{2,3,4,5,6,4,3,2}
 	};
 	int i1,i2,j1,j2;
+	int flagForErrors = 0;
+	int turn = 0;
+	while (1) {
+		char initCode, helpChoice;
+		// Menu code
+		generateMenu();
+		// Check for user input
+		scanf("/%c", &initCode);
+		// Necessary to avoid infinite loops.
+		getchar();
+		if (initCode == 'p') {
+			printf("Starting game... \n \n");
+			break;
+		} else if (initCode == 'h') {
+			printf("Help");
+		} else if (initCode == 'e') {
+			exit(0);
+		} else {
+			printf("Not expected.");
+		}
+	}
 	while (1) {
 		printBoard(8,8, board[0]);
 		printf("White Check: %d \n", lookForWhiteCheck(8,8,board[0]));
 		printf("Black Check: %d \n", lookForBlackCheck(8,8,board[0]));
+		printf("Turn: %d \n", turn);
 		printf("Insert next move: \n");
 		scanf("%1d%1d %1d%1d", &i1,&i2,&j1,&j2);
 		printf("i1 is: %d i2 is: %d j1 is: %d j2 is: %d \n",i1,i2,j1,j2);
@@ -47,14 +71,20 @@ int main(void) {
 			printf("There is no piece in position [%d,%d] \n",i1,i2);
 		} else {
 			int pieceCode = board[i1][i2];
-			int *possibleMoves = calculateMovesPiece(8, 8, board[0], i1, i2, pieceCode);
-			int lengthMovesArr = possibleMoves[0] - 2;
-			if (checkIfMoveIsIn(j1,j2,possibleMoves,lengthMovesArr)) {
-				movePiece(i1,i2,j1,j2,board[0],pieceCode);
+			if (turn == 0 && pieceCode >= 7) {
+				printf("It's white's turn, can't move black pieces.\n");
+			} else if (turn == 1 && pieceCode < 7) {
+				printf("It's black's turn, can't move white pieces.\n");
 			} else {
-				printf("The inserted move is not allowed!\n");
+				int *possibleMoves = calculateMovesPiece(8, 8, board[0], i1, i2, pieceCode);
+				int lengthMovesArr = possibleMoves[0] - 2;
+				if (checkIfMoveIsIn(j1,j2,possibleMoves,lengthMovesArr)) {
+					movePiece(i1,i2,j1,j2,board[0],pieceCode, &turn);
+				} else {
+					printf("The inserted move is not allowed!\n");
+				}
+				free(possibleMoves);
 			}
-			free(possibleMoves);
 		}
 	}
 	return 0;
--- code/movePieces.c
+++ code/movePieces.c
@@ -13,7 +13,12 @@ int * calculateAllowedMovesKnight(int rows, int cols, int *board, int rowPositio
 
 
 // Logic for moving a piece from initial position ([initRow, initColumn]) to end position ([endRow,endColumn])
-void movePiece(int initRow, int initColumn, int endRow, int endColumn, int * board, int code) {
+void movePiece(int initRow, int initColumn, int endRow, int endColumn, int * board, int code, int * turn) {
     board[initRow * 8 + initColumn] = 0;
     board[endRow * 8 + endColumn] = code;
+    if (code >= 7) {
+        *turn -= 1;
+    } else if (code < 7 && code > 0) {
+        *turn += 1;
+    }
 }
